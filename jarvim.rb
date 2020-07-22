3c0f575dade2c8c13bf6be648a3ff563e7ebb81a
Raphael <glepnir@gopherhub.org>
2020-07-22 18:23:20 +0800

    Bump version to 0.1.7

--- jarvim.rb
+++ jarvim.rb
@@ -5,7 +5,7 @@ class Jarvim < Formula
   desc "Generate a module vim configruation like a VIM PRO"
   homepage "https://github.com/glepnir/jarvim"
   url "https://github.com/glepnir/jarvim/releases/download/v0.1.7/jarvim-amd64-darwin.tar.gz"
-  sha256 "503e8377bfad145a0012d9dd821ef389616271d962db28dbb5cf8b9c6fcf8dc0"
+  sha256 "f2c3fb285a97ae11540f8673606609184f4935aae817e697e7a28469e1bae7e5"
   license "BSD3"
 
   # depends_on "cmake" => :build
