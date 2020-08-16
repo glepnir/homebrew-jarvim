fcafa499aa3d00a3f46f1a89dbe68d11f15eba1b
Raphael <glepnir@gopherhub.org>
2020-08-16 23:29:00 +0800

    update

--- jarvim.rb
+++ jarvim.rb
@@ -4,7 +4,7 @@
 class Jarvim < Formula
   desc "Generate a module vim configruation like a VIM PRO"
   homepage "https://github.com/glepnir/jarvim"
-  url "https://github.com/glepnir/jarvim/releases/download/v0.1.9/jarvim-amd64-darwin.tar.gz"
+  url "https://github.com/glepnir/jarvim/releases/download/v0.2.0/jarvim-amd64-darwin.tar.gz"
   sha256 "0f427a31aa5172ced7c9795f377f1096598bb5120331bc75d48eab168f8b8212"
   license "BSD3"
 
